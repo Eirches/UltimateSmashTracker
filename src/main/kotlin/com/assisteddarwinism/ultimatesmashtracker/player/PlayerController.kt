2ee6dd41ca07e7e3b8fd714cabb7665cbe73d175
Noah McClelland <nmcclelland@captechventures.com>
2018-12-17 12:33:07 -0500

    added GET /players/me and GET /players/{playerId}

--- src/main/kotlin/com/assisteddarwinism/ultimatesmashtracker/player/PlayerController.kt
+++ src/main/kotlin/com/assisteddarwinism/ultimatesmashtracker/player/PlayerController.kt
@@ -1,6 +1,7 @@
 package com.assisteddarwinism.ultimatesmashtracker.player
 
 import com.assisteddarwinism.ultimatesmashtracker.auth.TokenValidator
+import com.assisteddarwinism.ultimatesmashtracker.auth.repository.token.TokenRepository
 import com.assisteddarwinism.ultimatesmashtracker.model.exception.ResourceNotFoundException
 import com.assisteddarwinism.ultimatesmashtracker.player.model.Player
 import com.assisteddarwinism.ultimatesmashtracker.player.repository.PlayerDTO
@@ -20,22 +21,36 @@ class PlayerController {
     @Autowired
     lateinit var playerRepository: PlayerRepository
 
+    @Autowired
+    lateinit var tokenRepository: TokenRepository
+
     @GetMapping
-    fun getPlayers(@RequestHeader("X-AuthToken") token: String) : List<Player> {
+    fun getPlayers(@RequestHeader("X-AuthToken") token: String): List<Player> {
         tokenValidator.checkTokenValid(token)
 
         return playerRepository.findAll().map { it -> Player(it.id!!, it.name) }
     }
 
     @GetMapping("/{playerId}")
-    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-    fun getPlayer(@RequestHeader("X-AuthToken") token: String, @PathVariable("playerId") playerId: Long){
+    fun getPlayer(@RequestHeader("X-AuthToken") token: String, @PathVariable("playerId") playerId: Long): Player {
         tokenValidator.checkTokenValid(token)
+        try {
+            return getPlayers(token).first { it.id == playerId }
+        } catch (e: NoSuchElementException) {
+            throw ResourceNotFoundException()
+        }
+    }
+
+    @GetMapping("/me")
+    fun getMe(@RequestHeader("X-AuthToken") token: String): Player {
+        tokenValidator.checkTokenValid(token)
+        var tokenDTO = tokenRepository.findTokenDTOByToken(token)
+        return getPlayers(token).first { it.id == tokenDTO.id }
     }
 
     @GetMapping("/{playerId}/matches")
     @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-    fun getPlayerMatches(@RequestHeader("X-AuthToken") token: String, @PathVariable("playerId") playerId: Long){
+    fun getPlayerMatches(@RequestHeader("X-AuthToken") token: String, @PathVariable("playerId") playerId: Long) {
         tokenValidator.checkTokenValid(token)
     }
 
@@ -43,9 +58,9 @@ class PlayerController {
     fun deletePlayer(@RequestHeader("X-AuthToken") token: String, @PathVariable("playerId") playerId: Long) {
         tokenValidator.checkTokenAdmin(token)
 
-        if(!playerRepository.existsById(playerId)) throw ResourceNotFoundException()
+        if (!playerRepository.existsById(playerId)) throw ResourceNotFoundException()
         val storedPlayer = playerRepository.findById(playerId).get()
-        if(storedPlayer.isDeleted) throw ResourceNotFoundException()
+        if (storedPlayer.isDeleted) throw ResourceNotFoundException()
 
         val updatedPlayer = PlayerDTO(storedPlayer.name, storedPlayer.admin, true, storedPlayer.id)
         playerRepository.save(updatedPlayer)
